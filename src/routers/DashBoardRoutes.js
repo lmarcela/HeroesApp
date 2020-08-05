e14069ef14cd32e7c34e9da9b15fc5076efde2cb
Marcela Malaver <marcela9409@gmail.com>
2020-08-04 19:52:47 -0500

    Configuracion de routers + ajustes en package.json

---
+++ src/routers/DashBoardRoutes.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Navbar } from "../components/ui/NavBar";
+import { Switch, Redirect } from "react-router-dom";
+import { MarvelScreen } from "../components/marvel/MarvelScreen";
+import { HeroScreen } from "../components/heroes/HeroScreen";
+import { DcScreen } from "../components/dc/DcScreen";
+import { Route } from "react-router-dom";
+
+export const DashBoardRoutes = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Switch>
+          <Route exact path="/marvel" component={MarvelScreen} />
+          <Route exact path="/heroe/:heroeId" component={HeroScreen} />
+          <Route exact path="/dc" component={DcScreen} />
+          <Redirect to="/marvel" />
+        </Switch>
+      </div>
+    </>
+  );
+};
