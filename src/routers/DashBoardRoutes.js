fc693bd174d6e280e22d695d5cbac99cd2a2a291
Marcela Malaver <marcela9409@gmail.com>
2020-08-06 12:35:58 -0500

    SearchComponent en la barra de navegacion y con uso de customHook de useForm

--- src/routers/DashBoardRoutes.js
+++ src/routers/DashBoardRoutes.js
@@ -5,6 +5,7 @@ import { MarvelScreen } from "../components/marvel/MarvelScreen";
 import { HeroScreen } from "../components/heroes/HeroScreen";
 import { DcScreen } from "../components/dc/DcScreen";
 import { Route } from "react-router-dom";
+import { SearchScreen } from "../components/search/SearchScreen";
 
 export const DashBoardRoutes = () => {
   return (
@@ -15,6 +16,7 @@ export const DashBoardRoutes = () => {
           <Route exact path="/marvel" component={MarvelScreen} />
           <Route exact path="/hero/:heroeId" component={HeroScreen} />
           <Route exact path="/dc" component={DcScreen} />
+          <Route exact path="/search" component={SearchScreen} />
           <Redirect to="/marvel" />
         </Switch>
       </div>
