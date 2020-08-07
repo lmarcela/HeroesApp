9694664066aef3185bf2590444820b1216455665
Marcela Malaver <marcela9409@gmail.com>
2020-08-07 14:12:55 -0500

    Login de usuario

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../auth/AuthContext";
+import { types } from "../../types/types";
 
-export const LoginScreen = ({history}) => {
+export const LoginScreen = ({ history }) => {
+  const { dispatch } = useContext(AuthContext);
+
   const handleLogin = () => {
+    dispatch({
+      type: types.login,
+      payload: {
+        name: "Marcela"
+      }
+    });
     history.replace('/');
   };
   return (
